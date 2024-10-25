cfa86ba3e8217b458fa6095b35e5d015f9048a7e
Victor Prado <vmpradob@gmail.com>
2024-10-25 10:06:35 -0500

    increase memory limit for the project

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+ini_set('memory_limit', '512M');
 
 /*
 |--------------------------------------------------------------------------
